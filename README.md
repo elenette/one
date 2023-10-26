ddfaa05d43cd1fbc598a86d19196dfa89995b32f
elenette <148490688+elenette@users.noreply.github.com>
2023-10-26 15:26:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# one
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/elenette/one)
---
+++ index.js
@@ -0,0 +1,10 @@
+var a = 0;
+var list = [1, 2, 3, 4, 5, 10];
+var my_id = document.getElementById('my_id');
+var count = 0;
+
+function f() {
+  count++;
+  console.log(count);
+  button.textContent = "I've been clicked " + count + ' times';
+}
